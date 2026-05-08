e6976449cdd168fad042ea6cf8b174a7c0316579
Anshuman1907 <201937@kit.ac.in>
2026-05-09 02:10:28 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
 });
