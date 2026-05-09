742e4bf3f2ed0fcd9b364ca464198c6e193edbe9
Anshuman1907 <201937@kit.ac.in>
2026-05-09 23:42:08 +0530

    Remove obsolete Character-BPhBPU7Y.js file from the dist/assets directory.

---
+++ dist/assets/resumeSmoothScroll-ihtiuWgR.js
@@ -0,0 +1,1 @@
+import{a as e,n as t}from"./Navbar-BDM8NvtB.js";function n(){document.body.style.overflowY=`auto`;let n=document.getElementsByTagName(`main`)[0];n&&n.classList.add(`main-active`),t&&t.paused(!1),e.refresh()}export{n as t};
